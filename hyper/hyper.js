02d185172a04542899bfa0b2ea4e078479fb18e9
Bradley Wojcik <bradleycwojcik@gmail.com>
2022-06-09 23:50:38 -0400

    updated hyper config and starship config

--- hyper/hyper.js
+++ hyper/hyper.js
@@ -3,94 +3,22 @@
 // See https://hyper.is#cfg for all currently supported options.
 
 module.exports = {
+  // prefer default configs except
   config: {
-    // choose either `'stable'` for receiving highly polished,
-    // or `'canary'` for less polished but more frequent updates
-    updateChannel: 'stable',
-
-    // default font size in pixels for all tabs
     fontSize: 14,
-
-    // font family with optional fallbacks
     fontFamily: '"Fira Code", monospace',
-
-    // default font weight: 'normal' or 'bold'
-    fontWeight: 'normal',
-
-    // font weight for bold characters: 'normal' or 'bold'
-    fontWeightBold: 'bold',
-
-    // line height as a relative unit
-    lineHeight: 1,
-
-    // letter spacing as a relative unit
-    letterSpacing: 0,
-
-    // terminal cursor background color and opacity (hex, rgb, hsl, hsv, hwb or cmyk)
-    // cursorColor: 'rgba(248,28,229,0.8)',
-
-    // terminal text color under BLOCK cursor
-    cursorAccentColor: '#000',
-
-    // `'BEAM'` for |, `'UNDERLINE'` for _, `'BLOCK'` for █
-    cursorShape: 'BLOCK',
-
-    // set to `true` (without backticks and without quotes) for blinking cursor
-    cursorBlink: false,
-
-    // color of the text
-    // foregroundColor: '#fff',
-
-    opacity: 0.925,
-    // terminal background color
-    // opacity is only supported on macOS
-    // backgroundColor: 'rgba(256, 256, 256, 0.95)',
-
-    // terminal selection color
-    selectionColor: 'rgba(248,28,229,0.3)',
-
-    // border color (window, tabs)
-    // borderColor: '#333',
-
-    // custom CSS to embed in the main window
-    css: '',
-
-    // custom padding (CSS format, i.e.: `top right bottom left`)
-    padding: '12px 14px',
-
-    // the shell to run when spawning a new session (i.e. /usr/local/bin/fish)
-    // if left empty, your system's login shell will be used by default
-    shell: '/bin/zsh',
-
-    // for setting shell arguments (i.e. for using interactive shellArgs: `['-i']`)
-    // by default `['--login']` will be used
-    shellArgs: ['--login'],
-
-    // for environment variables
-    env: {},
-
-    // if `true` (without backticks and without quotes), selected text will automatically be copied to the clipboard
     copyOnSelect: true,
-
-    // if `true` (without backticks and without quotes), hyper will be set as the default protocol client for SSH
-    defaultSSHApp: true,
-
-    // if `true` (without backticks and without quotes), on right click selected text will be copied or pasted if no
-    // selection is present (`true` by default on Windows and disables the context menu feature)
-    quickEdit: false,
-
-    // for advanced config flags please refer to https://hyper.is/#cfg
   },
 
   // a list of plugins to fetch and install from npm
   // format: [@org/]project[#version]
   plugins: [
     "hyper-font-ligatures",
-    "hyper-quit",
-    "hyper-drop-file",
     "hyper-github-dark-dimmed",
+    "hyper-transparent-dynamic",
     "hyperterm-paste",
-    "hyper-opacity"
+    "hyperterm-close-on-left",
+    "hyper-quit",
   ],
 
   // in development, you can create a directory under
@@ -98,8 +26,9 @@ module.exports = {
   // to load it and avoid it being `npm install`ed
   localPlugins: [],
 
+  // keymap overrides
   keymaps: {
-    // Example
-    // 'window:devtools': 'cmd+alt+o',
+    'window:reload': 'command+r',
+    'window:reloadFull': 'command+shift+r'
   },
 };
