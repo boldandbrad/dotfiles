ee2bf52219929f92775968762534650087e28aaa
Bradley Wojcik <bradleycwojcik@gmail.com>
2022-11-16 09:24:10 -0500

    update prep script

--- prep.sh
+++ prep.sh
@@ -2,7 +2,7 @@
 
 # intended for remote use. clones this repo and does some basic system setup.
 
-REPOSITORY="git://github.com/boldandbrad/dotfiles.git"
+REPOSITORY="https://github.com/boldandbrad/dotfiles.git"
 
 function pull_dotfiles {
   # TODO: check for git
