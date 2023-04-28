5b2fa5e8b0a9d7d5eaade3e1b889b8b68c29cf7c
Bradley Wojcik <bradleycwojcik@gmail.com>
2023-04-27 23:26:17 -0400

    update macos dockutil setup

--- macos/dockutil/install.zsh
+++ macos/dockutil/install.zsh
@@ -7,21 +7,18 @@ dockutil --no-restart --remove all
 
 dockutil --no-restart --add "/System/Applications/Photos.app"
 dockutil --no-restart --add "/System/Applications/Calendar.app"
-dockutil --no-restart --add "/Applications/Bear.app"
+dockutil --no-restart --add "/Applications/Obsidian.app"
 dockutil --no-restart --add "/Applications/Todoist.app"
-dockutil --no-restart --add "/Applications/Canary Mail.app"
 dockutil --no-restart --add "/System/Applications/Mail.app"
 dockutil --no-restart --add "/Applications/Parcel.app"
 dockutil --no-restart --add "/Applications/Dashlane.app"
 dockutil --no-restart --add "/Applications/Safari.app"
+dockutil --no-restart --add "/Applications/DuckDuckGo.app"
 dockutil --no-restart --add "/Applications/Spotify.app"
 dockutil --no-restart --add "/System/Applications/Messages.app"
-dockutil --no-restart --add "/Applications/Signal.app"
 dockutil --no-restart --add "/Applications/Discord.app"
 dockutil --no-restart --add "/Applications/Hyper.app"
 dockutil --no-restart --add "/Applications/VSCodium.app"
-dockutil --no-restart --add "/Applications/Linear.app"
-dockutil --no-restart --add "/Applications/Numbers.app"
 
 killall Dock
 
