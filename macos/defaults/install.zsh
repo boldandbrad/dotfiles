f287c671ffa33295cee174b8fcde2c4bd88c6b03
Bradley Wojcik <bradleycwojcik@gmail.com>
2023-04-27 23:37:53 -0400

    start macos defaults

--- macos/defaults/install.zsh
+++ macos/defaults/install.zsh
@@ -6,3 +6,6 @@ echo "Configuring macOS Preferences..."
 apply-user-defaults defaults.yaml
 
 killall Dock
+
+defaults write -globalDomain NSUserKeyEquivalents -dict-add "Duplicate Tab" '@k'
+defaults write com.apple.Safari NSUserKeyEquivalents -dict-add "Duplicate Tab" '@k'
