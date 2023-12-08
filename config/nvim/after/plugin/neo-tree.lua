cc7bf92019cb2cd0418b0bd552bee4f29bcbd9d7
Bradley Wojcik <bradleycwojcik@gmail.com>
2023-12-08 15:20:52 -0500

    update neovim config with new options and plugins

---
+++ config/nvim/after/plugin/neo-tree.lua
@@ -0,0 +1,16 @@
+require('neo-tree').setup({
+    buffers = {
+        follow_current_file = {
+            enabled = true,
+        },
+    },
+    filesystem = {
+        follow_current_file = {
+            enabled = true,
+        }
+    },
+    window = {
+        position = "right",
+        width = 32,
+    },
+})
--- config/nvim/lua/boldandbrad/lazy.lua
+++ config/nvim/lua/boldandbrad/lazy.lua
@@ -14,33 +14,46 @@ vim.opt.rtp:prepend(lazypath)
 
 -- lazy plugins
 require("lazy").setup({
-	-- github dark theme
-	{
-		'projekt0n/github-nvim-theme',
-		lazy = false, -- make sure we load this during startup
-		priority = 1000, -- make sure to load this before all the other start plugins
-		config = function()
-			require('github-theme').setup({})
-			vim.cmd('colorscheme github_dark_default')
-		end,
-	},
-	-- telescope
-	{
-		'nvim-telescope/telescope.nvim',
-		tag = '0.1.5',
-		dependencies = { 'nvim-lua/plenary.nvim' },
-	},
-	-- treesitter
-	{
-		'nvim-treesitter/nvim-treesitter',
-		build = ':TSUpdate',
-	},
-	-- lsp
-	{'williamboman/mason.nvim'},
-	{'williamboman/mason-lspconfig.nvim'},
-	{'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'},
-	{'neovim/nvim-lspconfig'},
-	{'hrsh7th/cmp-nvim-lsp'},
-	{'hrsh7th/nvim-cmp'},
-	{'L3MON4D3/LuaSnip'},
+    -- github dark theme
+    {
+        'projekt0n/github-nvim-theme',
+        lazy = false, -- make sure we load this during startup
+        priority = 1000, -- make sure to load this before all the other start plugins
+        config = function()
+            require('github-theme').setup({})
+            vim.cmd('colorscheme github_dark_default')
+        end,
+    },
+    -- telescope
+    {
+        'nvim-telescope/telescope.nvim',
+        tag = '0.1.5',
+        dependencies = { 'nvim-lua/plenary.nvim' },
+    },
+    -- treesitter
+    {
+        'nvim-treesitter/nvim-treesitter',
+        build = ':TSUpdate',
+    },
+    -- neo-tree
+    {
+        "nvim-neo-tree/neo-tree.nvim",
+        branch = "v3.x",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+            "MunifTanjim/nui.nvim",
+            -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+        },
+    },
+    -- gitsigns
+    {"lewis6991/gitsigns.nvim"},
+    -- lsp
+    {'williamboman/mason.nvim'},
+    {'williamboman/mason-lspconfig.nvim'},
+    {'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'},
+    {'neovim/nvim-lspconfig'},
+    {'hrsh7th/cmp-nvim-lsp'},
+    {'hrsh7th/nvim-cmp'},
+    {'L3MON4D3/LuaSnip'},
 })
