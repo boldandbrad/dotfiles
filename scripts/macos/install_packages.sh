5c401a73dcd7851b6734c4090da61c65e387c5e5
Bradley Wojcik <bradleycwojcik@gmail.com>
2023-12-16 23:46:28 -0500

    consolidate macOS homebrew scripts

--- scripts/macos/install_packages.sh
+++ scripts/macos/install_packages.sh
@@ -7,8 +7,20 @@
 # See brew/Brewfile for a list of packages to be installed.    #
 ################################################################
 
-# install homebrew
-$DOTFILES/scripts/macos/brew/install.sh
+# install homebrew if not already installed
+echo -e "\nChecking for Homebrew..."
+if ! type brew &>/dev/null; then
+  echo "Installing Homebrew..."
+  # nosemgrep
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" || exit 1
+  echo -e "${GREEN_B}Homebrew is now installed!${RESET}"
+else
+  echo -e "${GREEN_B}Homebrew is already installed!${RESET}"
+fi
+
+# configure homebrew
+brew developer off
+brew analytics off
 
 # install apps and packages from brewfile via homebrew bundle
 echo
