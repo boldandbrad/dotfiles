3513eed9efae41196e6ebfad8232f11c586ccf4b
Bradley Wojcik <bradleycwojcik@gmail.com>
2023-12-20 09:13:32 -0500

    add tmux vim plugin

--- config/nvim/lua/boldandbrad/lazy.lua
+++ config/nvim/lua/boldandbrad/lazy.lua
@@ -61,4 +61,6 @@ require("lazy").setup({
     {'hrsh7th/cmp-nvim-lsp'},
     {'hrsh7th/nvim-cmp'},
     {'L3MON4D3/LuaSnip'},
+    -- vim-tmux-navigator
+    {"christoomey/vim-tmux-navigator", lazy = false},
 })
