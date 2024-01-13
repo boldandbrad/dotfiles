2e0b495492ebf9f14adaf99c13a6160af38d2eba
Bradley Wojcik <bradleycwojcik@gmail.com>
2024-01-13 11:37:44 -0500

    add nvim cmp, fidget, and basic diagnostics

--- config/nvim/lua/boldandbrad/plugins/lsp.lua
+++ config/nvim/lua/boldandbrad/plugins/lsp.lua
@@ -2,10 +2,23 @@ return {
   {
     "neovim/nvim-lspconfig",
     dependencies = {
+      -- mason
       "williamboman/mason.nvim",
       "williamboman/mason-lspconfig.nvim",
+      -- completions
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      "hrsh7th/cmp-cmdline",
+      "hrsh7th/nvim-cmp",
+      -- snippet engine
+      "L3MON4D3/LuaSnip",
+      "saadparwaiz1/cmp_luasnip",
+      -- lsp notifications
+      "j-hui/fidget.nvim",
     },
     config = function()
+      require("fidget").setup()
       require("mason").setup()
       require("mason-lspconfig").setup({
         ensure_installed = {
@@ -15,15 +28,61 @@ return {
         handlers = {
           function (server_name)
             require("lspconfig")[server_name].setup {}
+          end,
+          -- handle undefined global vim
+          ["lua_ls"] = function ()
+            local lspconfig = require("lspconfig")
+            lspconfig.lua_ls.setup {
+              settings = {
+                Lua = { diagnostics = { globals = { "vim" } } }
+              }
+            }
           end
+        }
+      })
+      -- completions
+      local cmp = require("cmp")
+      local cmp_select = { behavior = cmp.SelectBehavior.Select }
+      cmp.setup({
+        snippet = {
+          -- required
+          expand = function(args)
+            require("luasnip").lsp_expand(args.body)
+          end
+        },
+        window = {},
+        mapping = cmp.mapping.preset.insert({
+          -- recommended
+          ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+          ['<C-f>'] = cmp.mapping.scroll_docs(4),
+          ['<C-Space>'] = cmp.mapping.complete(),
+          ['<C-e>'] = cmp.mapping.abort(),
+          ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+          -- prime
+          ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+          ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+          -- ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+        }),
+        sources = cmp.config.sources({
+          { name = "nvim_lsp" },
+          { name = "luasnip" },
+          { name = "buffer" },
+        }),
+      })
+      -- diagnostics
+      -- TODO: read up on this
+      vim.diagnostic.config({
+        virtual_text = true,
+        update_in_insert = true,
+        float = {
+          focusable = false,
+          style = "minimal",
+          border = "rounded",
+          source = "always",
+          header = "",
+          prefix = "",
         }
       })
     end
   },
-
-
-  -- {'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'},
-  -- {'hrsh7th/cmp-nvim-lsp'},
-  -- {'hrsh7th/nvim-cmp'},
-  -- {'L3MON4D3/LuaSnip'},
 }
---
+++ config/nvim/lua/boldandbrad/plugins/snippets.lua
@@ -0,0 +1,4 @@
+return {
+   -- TODO: figure out snippets
+   "L3MON4D3/LuaSnip",
+}
