bf988d5794860c64ebf03605bd7708672de85857
Bradley Wojcik <bradleycwojcik@gmail.com>
2024-01-15 12:45:14 -0500

    nvim update dashboard and telescope keymaps

--- config/nvim/lua/boldandbrad/plugins/dashboard.lua
+++ config/nvim/lua/boldandbrad/plugins/dashboard.lua
@@ -1,7 +1,15 @@
 return {
   "goolord/alpha-nvim",
   config = function()
-    -- TODO: update config to show correct keymaps
-    require("alpha").setup(require("alpha.themes.dashboard").config)
+    local alpha = require("alpha")
+    local dashboard = require("alpha.themes.dashboard")
+    dashboard.section.buttons.val = {
+      dashboard.button("SPC f f", "  Find files", ":Telescope find_files <CR>"),
+      dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
+      dashboard.button("SPC f r", "  Recent files", ":Telescope oldfiles <CR>"),
+      dashboard.button("SPC f t", "󱎸  Find text", ":Telescope live_grep <CR>"),
+      dashboard.button("q", "  Quit Neovim", ":qa<CR>"),
+    }
+    alpha.setup(dashboard.opts)
   end,
 }
--- config/nvim/lua/boldandbrad/plugins/finder.lua
+++ config/nvim/lua/boldandbrad/plugins/finder.lua
@@ -5,9 +5,10 @@ return {
   config = function()
     local builtin = require("telescope.builtin")
     vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
-    vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+    vim.keymap.set("n", "<leader>ft", builtin.live_grep, {})
     vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
     vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
+    vim.keymap.set("n", "<leader>fr", builtin.oldfiles, {})
     -- find string
     vim.keymap.set("n", "<leader>fs", function()
       builtin.grep_string({ search = vim.fn.input("Grep > ") });
