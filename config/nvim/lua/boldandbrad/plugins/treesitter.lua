0c589fd6bdc8b672a5a1b735cd367c393d2b3e9a
Bradley Wojcik <bradleycwojcik@gmail.com>
2024-01-20 22:44:55 -0500

    neovim - add python to treesitter config

--- config/nvim/lua/boldandbrad/plugins/treesitter.lua
+++ config/nvim/lua/boldandbrad/plugins/treesitter.lua
@@ -19,6 +19,7 @@ return {
           "vim",
           "vimdoc",
           -- custom
+          "python",
           "rust",
         },
         incremental_selection = {
@@ -74,13 +75,13 @@ return {
               enable = true,
               swap_next = {
                 ["<leader>na"] = "@parameter.inner", -- swap parameters/argument with next
-                ["<leader>n:"] = "@property.outer", -- swap object property with next
-                ["<leader>nm"] = "@function.outer", -- swap function with next
+                ["<leader>n:"] = "@property.outer",  -- swap object property with next
+                ["<leader>nm"] = "@function.outer",  -- swap function with next
               },
               swap_previous = {
                 ["<leader>pa"] = "@parameter.inner", -- swap parameters/argument with prev
-                ["<leader>p:"] = "@property.outer", -- swap object property with prev
-                ["<leader>pm"] = "@function.outer", -- swap function with previous
+                ["<leader>p:"] = "@property.outer",  -- swap object property with prev
+                ["<leader>pm"] = "@function.outer",  -- swap function with previous
               },
             },
             move = {
