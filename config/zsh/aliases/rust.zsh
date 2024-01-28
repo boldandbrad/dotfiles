f612e50fab464a289692e247b824034effe24f6b
Bradley Wojcik <bradleycwojcik@gmail.com>
2024-01-28 17:00:28 -0500

    add cargo/rust aliases

---
+++ config/zsh/aliases/rust.zsh
@@ -0,0 +1,10 @@
+################################################################
+# rust.zsh - rust/cargo aliases                                #
+################################################################
+# -> dotbot symlinks this file into ~/.config/zsh/aliases/     #
+#                                                              #
+# Sourced from .zshrc                                          #
+# Setup rust/cargo zsh aliases.                                #
+################################################################
+
+alias cgr="cargo run"
