832a0f722b352696d5c9f7c53ead65f14fa7b961
Bradley Wojcik <bradleycwojcik@gmail.com>
2024-06-16 23:01:41 -0400

    add golang shell aliases

---
+++ config/zsh/aliases/go.zsh
@@ -0,0 +1,12 @@
+################################################################
+# go.zsh - golang aliases                                      #
+################################################################
+# -> dotbot symlinks this file into ~/.config/zsh/aliases/     #
+#                                                              #
+# Sourced from .zshrc                                          #
+# Setup golang zsh aliases.                                    #
+################################################################
+
+alias gor="go run ."
+alias gog="go get"
+alias gob="go build"
