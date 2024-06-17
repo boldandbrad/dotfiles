2630af322162e11942fb0fc940b542025b8cb161
Bradley Wojcik <bradleycwojcik@gmail.com>
2024-06-17 13:55:27 -0400

    start using lazygit

---
+++ config/nvim/lua/boldandbrad/plugins/lazygit.lua
@@ -0,0 +1,18 @@
+return {
+  "kdheepak/lazygit.nvim",
+  cmd = {
+    "LazyGit",
+    "LazyGitConfig",
+    "LazyGitCurrentFile",
+    "LazyGitFilter",
+    "LazyGitFilterCurrentFile",
+  },
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+  },
+  -- setting the keybinding for LazyGit with 'keys' is recommended in
+  -- order to load the plugin when the command is run for the first time
+  keys = {
+    { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" }
+  }
+}
