8727fac78d3955d7b155b0b38f40f54581411003
Bradley Wojcik <bradleycwojcik@gmail.com>
2024-06-19 21:41:33 -0400

    nvim keymap cleanup

--- config/nvim/lua/boldandbrad/plugins/codeium.lua
+++ config/nvim/lua/boldandbrad/plugins/codeium.lua
@@ -2,8 +2,8 @@ return {
   "Exafunction/codeium.vim",
   event = "BufEnter",
   config = function()
-    -- Change "<C-g>" here to any keycode you like.
     vim.g.codeium_no_map_tab = 1
+    -- Change "<C-g>" here to any keycode you like.
     vim.keymap.set("i", "<C-g>", function() return vim.fn["codeium#Accept"]() end, { expr = true, silent = true })
     vim.keymap.set("i", "<c-;>", function() return vim.fn["codeium#CycleCompletions"](1) end,
       { expr = true, silent = true })
--- config/nvim/lua/boldandbrad/plugins/gitsigns.lua
+++ config/nvim/lua/boldandbrad/plugins/gitsigns.lua
@@ -2,12 +2,12 @@ return {
   "lewis6991/gitsigns.nvim",
   config = function()
     require("gitsigns").setup()
-    vim.keymap.set("n", "<leader>ghv", ":Gitsigns preview_hunk_inline <CR>")
-    vim.keymap.set("n", "<leader>ghs", ":Gitsigns stage_hunk <CR>")
-    vim.keymap.set("n", "<leader>ghu", ":Gitsigns undo_stage_hunk <CR>")
-    vim.keymap.set("n", "<leader>gbs", ":Gitsigns stage_buffer <CR>")
-    vim.keymap.set("n", "<leader>ghr", ":Gitsigns reset_hunk <CR>")
-    vim.keymap.set("n", "<leader>gbr", ":Gitsigns reset_buffer <CR>")
-    vim.keymap.set("n", "<leader>gd", ":Gitsigns diffthis <CR>")
-  end,
+    vim.keymap.set("n", "<leader>ghv", ":Gitsigns preview_hunk_inline <CR>", { desc = "Preview hunk" })
+    vim.keymap.set("n", "<leader>ghs", ":Gitsigns stage_hunk <CR>", { desc = "Stage hunk" })
+    vim.keymap.set("n", "<leader>ghu", ":Gitsigns undo_stage_hunk <CR>", { desc = "Undo stage hunk" })
+    vim.keymap.set("n", "<leader>gbs", ":Gitsigns stage_buffer <CR>", { desc = "Stage buffer" })
+    vim.keymap.set("n", "<leader>ghr", ":Gitsigns reset_hunk <CR>", { desc = "Reset hunk" })
+    vim.keymap.set("n", "<leader>gbr", ":Gitsigns reset_buffer <CR>", { desc = "Reset buffer" })
+    vim.keymap.set("n", "<leader>gd", ":Gitsigns diffthis <CR>", { desc = "Diff buffer" })
+  end
 }
