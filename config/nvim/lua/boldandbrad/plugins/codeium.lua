ddbeaee87e42d74618dc46dfb2da7e4b94f4f83f
Bradley Wojcik <bradleycwojcik@gmail.com>
2024-06-20 20:27:36 -0400

    nvim manually trigger codeium

--- config/nvim/lua/boldandbrad/plugins/codeium.lua
+++ config/nvim/lua/boldandbrad/plugins/codeium.lua
@@ -3,9 +3,10 @@ return {
   event = "BufEnter",
   config = function()
     vim.g.codeium_no_map_tab = 1
+    vim.g.codeium_manual = true
     -- Change "<C-g>" here to any keycode you like.
     vim.keymap.set("i", "<C-g>", function() return vim.fn["codeium#Accept"]() end, { expr = true, silent = true })
-    vim.keymap.set("i", "<c-;>", function() return vim.fn["codeium#CycleCompletions"](1) end,
+    vim.keymap.set("i", "<c-;>", function() return vim.fn["codeium#CycleOrComplete"]() end,
       { expr = true, silent = true })
     vim.keymap.set("i", "<c-,>", function() return vim.fn["codeium#CycleCompletions"](-1) end,
       { expr = true, silent = true })
