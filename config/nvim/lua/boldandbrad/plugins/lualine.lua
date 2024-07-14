9b29208b8697d330a04abcd512cc21aaa5431bea
Bradley Wojcik <bradleycwojcik@gmail.com>
2024-07-13 23:25:42 -0400

    modernize nvim lualine plugin setup

--- config/nvim/lua/boldandbrad/plugins/lualine.lua
+++ config/nvim/lua/boldandbrad/plugins/lualine.lua
@@ -9,21 +9,19 @@ return {
     "rmagatti/auto-session",
     "cbochs/grapple.nvim",
   },
-  config = function()
-    require("lualine").setup({
-      sections = {
-        lualine_c = {
-          require("auto-session.lib").current_session_name,
-          "filename",
-          "grapple",
-        }
-      },
-      options = {
-        icons_enabled = false,
-        component_separators = { left = "|", right = "|" },
-        section_separators = { left = "", right = "" },
-        globalstatus = true,
-      },
-    })
-  end,
+  opts = {
+    sections = {
+      lualine_c = {
+        require("auto-session.lib").current_session_name,
+        "filename",
+        "grapple",
+      }
+    },
+    options = {
+      icons_enabled = false,
+      component_separators = { left = "|", right = "|" },
+      section_separators = { left = "", right = "" },
+      globalstatus = true,
+    },
+  },
 }
