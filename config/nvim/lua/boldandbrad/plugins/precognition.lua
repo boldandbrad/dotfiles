fd8199fdfd1cd894e62f9e47f02aa32021d7187f
Bradley Wojcik <bradleycwojcik@gmail.com>
2024-07-15 19:02:14 -0400

    cleanup nvim precognition config

--- config/nvim/lua/boldandbrad/plugins/precognition.lua
+++ config/nvim/lua/boldandbrad/plugins/precognition.lua
@@ -9,7 +9,7 @@ return {
     startVisible = false,
   },
   keys = {
-    { "<leader>hp", mode = { "n", "x", "o" }, function() require("precognition").peek() end,   desc = "Precognition Peek" },
-    { "<leader>ht", mode = { "n", "x", "o" }, function() require("precognition").toggle() end, desc = "Precognition Toggle" },
+    { "<leader>hp", mode = { "n", "x", "o" }, "<cmd>Precognition peek<cr>",   desc = "Precognition Peek" },
+    { "<leader>ht", mode = { "n", "x", "o" }, "<cmd>Precognition toggle<cr>", desc = "Precognition Toggle" },
   }
 }
