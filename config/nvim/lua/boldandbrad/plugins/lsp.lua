d48e29f71e19a5f116cb0dba3e6924c4d4601340
Bradley Wojcik <bradleycwojcik@gmail.com>
2024-07-17 21:36:51 -0400

    nvim update lsp cmp and snippets config

--- config/nvim/lua/boldandbrad/plugins/lsp.lua
+++ config/nvim/lua/boldandbrad/plugins/lsp.lua
@@ -11,18 +11,15 @@ return {
     -- completions
     "hrsh7th/cmp-nvim-lsp",
     "hrsh7th/cmp-buffer",
-    "hrsh7th/cmp-path",
-    "hrsh7th/cmp-cmdline",
+    "hrsh7th/cmp-path", -- filesystem path completions
     "hrsh7th/nvim-cmp",
-    -- snippet engine
-    "L3MON4D3/LuaSnip",
-    "saadparwaiz1/cmp_luasnip",
     -- lsp notifications
     "j-hui/fidget.nvim",
   },
   config = function()
-    require("fidget").setup()
-    require("mason").setup()
+    require("mason").setup({
+      ui = { border = "rounded" }
+    })
     require("mason-lspconfig").setup({
       -- install language servers
       ensure_installed = {
@@ -41,7 +38,10 @@ return {
       -- setup language servers
       handlers = {
         function(server_name)
-          require("lspconfig")[server_name].setup {}
+          local capabilities = require("cmp_nvim_lsp").default_capabilities()
+          require("lspconfig")[server_name].setup {
+            capabilities = capabilities
+          }
         end,
         -- lua: handle undefined global "vim"
         ["lua_ls"] = function()
@@ -54,17 +54,32 @@ return {
         end,
       },
     })
+
     -- setup completions
     local cmp = require("cmp")
     local cmp_select = { behavior = cmp.SelectBehavior.Select }
     cmp.setup({
+      enabled = function()
+        -- disable completions in comments
+        local context = require "cmp.config.context"
+        -- keep command mode completion enabled when cursor is in a comment
+        if vim.api.nvim_get_mode().mode == "c" then
+          return true
+        else
+          return not context.in_treesitter_capture("comment")
+              and not context.in_syntax_group("Comment")
+        end
+      end,
       snippet = {
         -- required
         expand = function(args)
-          require("luasnip").lsp_expand(args.body)
+          vim.snippet.expand(args.body)
         end,
       },
-      window = {},
+      window = {
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered(),
+      },
       mapping = cmp.mapping.preset.insert({
         -- recommended
         ['<C-b>'] = cmp.mapping.scroll_docs(-4),
@@ -79,10 +94,11 @@ return {
       }),
       sources = cmp.config.sources({
         { name = "nvim_lsp" },
-        { name = "luasnip" },
         { name = "buffer" },
+        { name = "path" },
       }),
     })
+
     -- configure diagnostics
     vim.diagnostic.config({
       signs = false,
