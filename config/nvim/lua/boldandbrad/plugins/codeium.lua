d830b3963b43b5b1806141195172c894f900637c
Bradley Wojcik <bradleycwojcik@gmail.com>
2024-07-17 21:34:03 -0400

    nvim modernize codeium plugin setup

--- config/nvim/lua/boldandbrad/plugins/codeium.lua
+++ config/nvim/lua/boldandbrad/plugins/codeium.lua
@@ -4,16 +4,14 @@
 
 return {
   "Exafunction/codeium.vim",
-  event = "BufEnter",
-  config = function()
+  init = function()
     vim.g.codeium_no_map_tab = 1
     vim.g.codeium_manual = true
-    -- Change "<C-g>" here to any keycode you like.
-    vim.keymap.set("i", "<C-g>", function() return vim.fn["codeium#Accept"]() end, { expr = true, silent = true })
-    vim.keymap.set("i", "<c-;>", function() return vim.fn["codeium#CycleOrComplete"]() end,
-      { expr = true, silent = true })
-    vim.keymap.set("i", "<c-,>", function() return vim.fn["codeium#CycleCompletions"](-1) end,
-      { expr = true, silent = true })
-    vim.keymap.set("i", "<c-x>", function() return vim.fn["codeium#Clear"]() end, { expr = true, silent = true })
-  end
+  end,
+  keys = {
+    { "<C-g>", mode = "i", function() return vim.fn["codeium#Accept"]() end,             expr = true, silent = true },
+    { "<c-;>", mode = "i", function() return vim.fn["codeium#CycleOrComplete"]() end,    expr = true, silent = true },
+    { "<c-,>", mode = "i", function() return vim.fn["codeium#CycleCompletions"](-1) end, expr = true, silent = true },
+    { "<c-x>", mode = "i", function() return vim.fn["codeium#Clear"]() end,              expr = true, silent = true },
+  },
 }
