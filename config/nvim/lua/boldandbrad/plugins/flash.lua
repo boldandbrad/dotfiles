b1b9bb3356e54b0f61b1f67470d064c0bcf75972
Bradley Wojcik <bradleycwojcik@gmail.com>
2024-07-22 16:52:51 -0400

    nvim flash disable prompt

--- config/nvim/lua/boldandbrad/plugins/flash.lua
+++ config/nvim/lua/boldandbrad/plugins/flash.lua
@@ -5,7 +5,11 @@
 return {
   "folke/flash.nvim",
   event = "VeryLazy",
-  opts = {},
+  opts = {
+    prompt = {
+      enabled = false,
+    },
+  },
   keys = {
     { "s",     mode = { "n", "x", "o" }, function() require("flash").jump() end,              desc = "Flash" },
     { "S",     mode = { "n", "x", "o" }, function() require("flash").treesitter() end,        desc = "Flash Treesitter" },
