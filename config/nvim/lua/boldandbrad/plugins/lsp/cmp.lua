0c32cf940c9d5c73533a16cf1fc997872cafe416
Bradley Wojcik <bradleycwojcik@gmail.com>
2024-11-22 13:09:14 -0500

    neovim: do not auto select first cmp

--- config/nvim/lua/boldandbrad/plugins/lsp/cmp.lua
+++ config/nvim/lua/boldandbrad/plugins/lsp/cmp.lua
@@ -40,7 +40,7 @@ return {
         ['<C-f>'] = cmp.mapping.scroll_docs(4),
         ['<C-Space>'] = cmp.mapping.complete(),
         ['<C-e>'] = cmp.mapping.abort(),
-        ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+        ['<CR>'] = cmp.mapping.confirm({ select = false }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
         -- prime
         ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
         ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
