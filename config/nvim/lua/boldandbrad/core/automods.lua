194168c128a4ef88b63e4deeaf24479dd85419a2
Bradley Wojcik <bradleycwojcik@gmail.com>
2024-11-22 14:42:21 -0500

    neovim: add lsp action keymaps

--- config/nvim/lua/boldandbrad/core/automods.lua
+++ config/nvim/lua/boldandbrad/core/automods.lua
@@ -2,6 +2,25 @@
 -- nvim automods                                              --
 ----------------------------------------------------------------
 
+-- add lsp actions
+vim.api.nvim_create_autocmd("LspAttach", {
+  desc = "LSP Actions",
+  callback = function(event)
+    local opts = { buffer = event.buf }
+
+    vim.keymap.set("n", "K", "<cmd>lua vim.lsp.buf.hover()<cr>", opts)
+    vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
+    vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
+    vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
+    vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
+    vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
+    vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
+    vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
+    vim.keymap.set({ 'n', 'x' }, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
+    vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
+  end,
+})
+
 -- auto trim trailing whitespace on write
 vim.api.nvim_create_autocmd({ "BufWritePre" }, {
   pattern = { "*" },
