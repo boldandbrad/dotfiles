f29375ad5551950d0ec1d4a4e9dd7b7629998ef5
Bradley Wojcik <bradleycwojcik@gmail.com>
2024-11-24 12:43:25 -0500

    neovim: remove colorizer, update plugins

--- config/nvim/lua/boldandbrad/plugins/init.lua
+++ config/nvim/lua/boldandbrad/plugins/init.lua
@@ -2,11 +2,4 @@
 -- basic plugins                                              --
 ----------------------------------------------------------------
 
-return {
-  -- color code highlighter
-  {
-    "NvChad/nvim-colorizer.lua",
-    event = { "BufReadPre", "BufNewFile" },
-    config = true,
-  },
-}
+return {}
