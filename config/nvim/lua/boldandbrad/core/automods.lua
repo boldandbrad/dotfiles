a9edfe83ece16c8851b7b545cbf02decb1183f42
Bradley Wojcik <bradleycwojcik@gmail.com>
2024-12-04 08:40:32 -0500

    nvim: cleanup autocmds, add autocmd to highlight on yank

--- config/nvim/lua/boldandbrad/core/automods.lua
+++ config/nvim/lua/boldandbrad/core/automods.lua
@@ -2,27 +2,26 @@
 -- nvim automods                                              --
 ----------------------------------------------------------------
 
--- add lsp actions
 vim.api.nvim_create_autocmd("LspAttach", {
-  desc = "LSP Actions",
+  desc = "Define LSP Action mappings",
   callback = function(event)
     local opts = { buffer = event.buf }
 
     vim.keymap.set("n", "K", "<cmd>lua vim.lsp.buf.hover()<cr>", opts)
-    vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
-    vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
-    vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
-    vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
-    vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
-    vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
-    vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
-    vim.keymap.set({ 'n', 'x' }, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
-    vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
+    vim.keymap.set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<cr>", opts)
+    vim.keymap.set("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<cr>", opts)
+    vim.keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<cr>", opts)
+    vim.keymap.set("n", "go", "<cmd>lua vim.lsp.buf.type_definition()<cr>", opts)
+    vim.keymap.set("n", "gr", "<cmd>lua vim.lsp.buf.references()<cr>", opts)
+    vim.keymap.set("n", "gs", "<cmd>lua vim.lsp.buf.signature_help()<cr>", opts)
+    vim.keymap.set("n", "<F2>", "<cmd>lua vim.lsp.buf.rename()<cr>", opts)
+    vim.keymap.set({ "n", "x" }, "<F3>", "<cmd>lua vim.lsp.buf.format({async = true})<cr>", opts)
+    vim.keymap.set("n", "<F4>", "<cmd>lua vim.lsp.buf.code_action()<cr>", opts)
   end,
 })
 
--- auto trim trailing whitespace on write
-vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+vim.api.nvim_create_autocmd("BufWritePre", {
+  desc = "Auto trim trailing whitespace on write",
   pattern = { "*" },
   callback = function()
     local save_cursor = vim.fn.getpos(".")
@@ -31,28 +30,37 @@ vim.api.nvim_create_autocmd({ "BufWritePre" }, {
   end,
 })
 
--- auto format via lsp on write
-vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+vim.api.nvim_create_autocmd("BufWritePre", {
+  desc = "Auto format buffer via lsp on write",
   callback = function()
     vim.lsp.buf.format()
   end,
 })
 
--- return to last edit position when opening buffer
-vim.api.nvim_create_autocmd({ "BufReadPost" }, {
-  -- group = "bufcheck",
-  pattern = "*",
-  callback = function()
-    if vim.fn.line("'\"") > 0 and vim.fn.line("'\"") <= vim.fn.line("$") then
-      vim.fn.setpos('.', vim.fn.getpos("'\""))
-      vim.api.nvim_feedkeys('zz', 'n', true)
-    end
-  end,
-})
+-- auto-session handles this
+-- -- return to last edit position when opening buffer
+-- vim.api.nvim_create_autocmd({ "BufReadPost" }, {
+--   -- group = "bufcheck",
+--   pattern = "*",
+--   callback = function()
+--     if vim.fn.line("'\"") > 0 and vim.fn.line("'\"") <= vim.fn.line("$") then
+--       vim.fn.setpos('.', vim.fn.getpos("'\""))
+--       vim.api.nvim_feedkeys('zz', 'n', true)
+--     end
+--   end,
+-- })
 
--- start terminal in insert mode
-vim.api.nvim_create_autocmd('TermOpen', {
+vim.api.nvim_create_autocmd("TermOpen", {
+  desc = "Start terminal in insert mode",
   -- group   = "bufcheck",
   pattern = "*",
   command = "startinsert | set winfixheight"
+})
+
+vim.api.nvim_create_autocmd("TextYankPost", {
+  desc = "Highlight when yanking text",
+  group = vim.api.nvim_create_augroup("highlight-yank", { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end
 })
