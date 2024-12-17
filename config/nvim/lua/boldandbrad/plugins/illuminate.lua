47bf62958e2a4593f03a056fa2d293644235af34
Bradley Wojcik <bradleycwojcik@gmail.com>
2024-12-16 22:59:28 -0500

    nvim: do not illuminate in telescope prompt

--- config/nvim/lua/boldandbrad/plugins/illuminate.lua
+++ config/nvim/lua/boldandbrad/plugins/illuminate.lua
@@ -6,7 +6,10 @@ return {
   "RRethy/vim-illuminate",
   config = function()
     require("illuminate").configure({
-      delay = 100
+      delay = 100,
+      filetypes_denylist = {
+        "TelescopePrompt",
+      }
     })
   end
 }
