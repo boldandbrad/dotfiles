ea4fa8949c475d268d869bf0b8e436ab7654e805
boldandbrad <bradleycwojcik@gmail.com>
2025-04-09 21:09:15 -0400

    handle macOS dock setup via defaults instead of dockutil

--- util/macos/config_system.sh
+++ util/macos/config_system.sh
@@ -5,19 +5,13 @@ touch ~/.hushlogin
 
 # set desktop wallpaper
 # NOTE: this may cause macOS to prompt the user to provide their terminal emulator with home folder access
-osascript -e "tell application \"System Events\" to tell every desktop to set picture to \"~/Dotfiles/assets/wallpapers/catppuccin-waves-4k.png\" as POSIX file"
+osascript -e "tell application \"System Events\" to tell every desktop to set picture to \"~/Dotfiles/assets/wallpapers/apple-black-4k.png\" as POSIX file"
+echo "Set desktop wallpaper!"
 
-# setup macOS dock
-if type dockutil &>/dev/null; then
-  echo
-  echo "Configuring macOS Dock..."
-
-  dockutil --no-restart --remove all
-
-  killall Dock
-
-  echo "Dock has been setup!"
-fi
+# clear macOS dock
+defaults delete com.apple.dock persistent-apps
+killall Dock
+echo "Dock has been cleared!"
 
 # map capslock key to escape
 # courtesy of https://stackoverflow.com/a/46460200/19295049
@@ -25,3 +19,4 @@ if type hidutil &>/dev/null; then
   hidutil property --set '{"UserKeyMapping":[{"HIDKeyboardModifierMappingSrc":0x700000039,"HIDKeyboardModifierMappingDst":0x700000029}]}'
   launchctl load com.boldandbrad.CapslockEscape.plist
 fi
+echo "Mapped Caps Lock to Esc!"
