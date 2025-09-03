9c15d032bf76964b3fed9a85bef9fec483b061e5
boldandbrad <bradleycwojcik@gmail.com>
2025-09-02 22:53:39 -0400

    add neovim autocmd to auto change to project root

--- config/nvim/lua/config/autocmds.lua
+++ config/nvim/lua/config/autocmds.lua
@@ -5,6 +5,44 @@
 local autocmd = vim.api.nvim_create_autocmd
 local augroup = vim.api.nvim_create_augroup
 
+local prj_root_group = augroup("ProjectRoot", { clear = true })
+autocmd({ "VimEnter", "BufReadPost", "BufEnter" }, {
+  desc = "Auto change to project root",
+  group = prj_root_group,
+  pattern = "*",
+  callback = function()
+    local root_patterns = { ".git", "go.mod", "deno.json", "Cargo.toml", "pyproject.toml" }
+    local current_dir = vim.fn.expand("%:p:h")
+
+    local root_found = false
+    while not root_found do
+      local is_root = false
+      for _, root_pattern in ipairs(root_patterns) do
+        if vim.fn.glob(current_dir .. "/" .. root_pattern) ~= "" then
+          is_root = true
+        end
+      end
+
+      if is_root then
+        root_found = true
+      elseif current_dir == "/" then
+        break
+      else
+        current_dir = vim.fn.fnamemodify(current_dir, ":h")
+      end
+
+      if root_found then
+        if current_dir == vim.fn.getcwd() then
+          return
+        else
+          vim.cmd("cd " .. current_dir)
+          vim.notify("Changed to project root: " .. current_dir)
+        end
+      end
+    end
+  end
+})
+
 autocmd("LspAttach", {
   desc = "Define LSP Actions",
   callback = function(args)
