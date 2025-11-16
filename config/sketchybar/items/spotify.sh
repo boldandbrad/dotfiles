025fdc5749bd2466951c90ec5c2fd699ecf207fe
boldandbrad <bradleycwojcik@gmail.com>
2025-11-15 23:34:19 -0500

    [sketchybar] move spotify item to right of notch

--- config/sketchybar/items/spotify.sh
+++ config/sketchybar/items/spotify.sh
@@ -1,15 +1,17 @@
 #!/bin/sh
 
 # initialize
-sketchybar --add item spotify right
+sketchybar --add item spotify e
 
 # config
 SPOTIFY_EVENT="com.spotify.client.PlaybackStateChanged"
 sketchybar --add event spotify_change $SPOTIFY_EVENT
 sketchybar --set spotify label.max_chars=40
+sketchybar --set spotify scroll_texts=on
 sketchybar --set spotify icon.drawing=off
 sketchybar --set spotify script="$PLUGIN_DIR/spotify.sh"
 sketchybar --subscribe spotify spotify_change
+sketchybar --set spotify click_script="open -a Spotify"
 
 # overrides
 sketchybar --set spotify background.color=0x40cdd6f4
